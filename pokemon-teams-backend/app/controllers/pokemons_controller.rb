f024bcbc91b7b006fc94f616fe8ed966509b7799
janewysoki <jane.wysoki@gmail.com>
2020-12-20 14:05:35 -0500

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,13 @@
+class PokemonsController < ApplicationController
+
+    def index
+        pokemons = Pokemon.all 
+        render json: pokemons, include: [:trainer]
+    end
+
+    def show 
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: pokemon, include: [:trainer]
+    end
+
+end
